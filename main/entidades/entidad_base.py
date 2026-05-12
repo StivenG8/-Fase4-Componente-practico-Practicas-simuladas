2b7f7096820a4a30479de113b66530fdd9bb5a41
StivenG8 <stivenguerrero0102@gmail.com>
2026-05-12 15:31:46 -0500

    Delete main directory

--- main/entidades/entidad_base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Entidad(ABC):
-    """Clase abstracta que representa entidades generales del sistema."""
-    @abstractmethod
-    def validar(self):
-        """Método abstracto para validar el estado de la entidad."""
-        pass
--- main/excepciones/errores.py
+++
@@ -1,9 +0,0 @@
-"""Excepciones personalizadas para el dominio del sistema."""
-
-class ErrorValidacion(Exception):
-    """Excepción para errores de validación de datos."""
-    pass
-
-class ErrorReserva(Exception):
-    """Excepción para errores lógicos al crear o procesar una reserva."""
-    pass
